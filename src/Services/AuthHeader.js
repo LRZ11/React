e8e0b7efe4a15e3cc5718fe09ff792518815d9c7
Lee Zafra <leezafra11@gmail.com>
2023-04-24 14:52:15 +0800

    Authentication Services

---
+++ src/Services/AuthHeader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function authHeader{
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    
+    if(user && user.token){
+        return {Authorization: 'Bearer' + user.token};
+    }else{
+        return{};
+    }
+}
